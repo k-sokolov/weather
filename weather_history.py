b75d7c6315a67d46292b721d7d455a205c7e2c2c
k-sokolov <ksokolov@uos.de>
2018-04-05 20:57:05 +0300

    corrected

--- weather_history.py
+++ weather_history.py
@@ -4,26 +4,31 @@ from xml.etree import ElementTree
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 
-url = 'http://www.eurometeo.ru/belarus/minskaya-oblast/minsk/export/xml/data/'
 
-r = urllib.request.urlopen(url)
-data = r.read()
-tree = ElementTree.fromstring(data)
-temp_list, time_list = [], []
+def main():
+    url = 'http://www.eurometeo.ru/belarus/minskaya-oblast/minsk/export/xml/data/'
 
-for step in tree.findall('./city/step'):
-    time = step.find('datetime').text
-    temp = step.find('temperature').text
-    temp_list.append(temp)
-    time_list.append(time)
-    print(time, temp)
+    r = urllib.request.urlopen(url)
+    data = r.read()
+    tree = ElementTree.fromstring(data)
+    temp_list, time_list = [], []
 
-fig, ax = plt.subplots(figsize=(15, 10))
-fmt = '%Y-%m-%d %H:%M:%S'
-time_list = [datetime.datetime.strptime(i, fmt) for i in time_list]
-ax.xaxis.set_major_formatter(dates.DateFormatter(fmt))
-ax.plot_date(time_list, temp_list, linestyle='solid')
-plt.xticks(time_list, rotation=30, ha='right')
-ax.set_xlabel('time')
-ax.set_ylabel('temperature')
-plt.savefig('weather_history.png')
+    for step in tree.findall('./city/step'):
+        time = step.find('datetime').text
+        temp = step.find('temperature').text
+        temp_list.append(float(temp))
+        time_list.append(time)
+        print(time, temp)
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    fmt = '%Y-%m-%d %H:%M:%S'
+    time_list = [datetime.datetime.strptime(i, fmt) for i in time_list]
+    ax.xaxis.set_major_formatter(dates.DateFormatter(fmt))
+    ax.plot(time_list, temp_list, linestyle='solid')
+    plt.xticks(time_list, rotation=30, ha='right')
+    ax.set_xlabel('time')
+    ax.set_ylabel('temperature')
+    plt.savefig('weather_history.png')
+
+if __name__ == '__main__':
+    main()
